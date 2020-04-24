8dc7cfff2bcea74e3ecfb615165d6a37fa1c1f1f
Ashtonn77 <ashtonrbay@gmail.com>
2020-04-24 20:21:18 +0200

    added weather-app

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,23 @@
 const request = require('postman-request');
 
-const url = 'http://api.weatherstack.com/current?access_key=1faba77a2a4bf8abdfeca8e98c4e2483&query=37.8267,-122.4233';
+let place = 'Rome';
+const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + place +'.json?access_token=pk.eyJ1IjoiYXNodG9ubjc3IiwiYSI6ImNrOWVoNGJpZTAxdGozbnJ6YmZ4YTU3YmsifQ.URWLnzB_CFi-ArH9iLlShg&limit=1';
 
-request({ url: url }, (error, response) => {
-    const data = JSON.parse(response.body);
-    console.log(data.current)
+request({url:geoUrl, json:true}, (error, response) => {
+    const data = response.body;
+    const latitude = data.features[0].center[1];
+    const longitude = data.features[0].center[0]
+    console.log(`Latitude = ${latitude}`);
+    console.log(`Longitude = ${longitude}`);
+
 })
+
+
+
+// const url = 'http://api.weatherstack.com/current?access_key=1faba77a2a4bf8abdfeca8e98c4e2483&query=37.8267,-122.4233&units=m';
+
+// request({ url: url, json: true }, (error, response) => {
+//     const data = response.body.current;
+//     console.log(`${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. But it feels like ${data.feelslike} degrees out`);
+   
+// })
