9f07c986034a38c71c3b4e95b240b5d8190f388f
Ashtonn77 <ashtonrbay@gmail.com>
2020-04-26 18:19:22 +0200

    added app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,23 +1,36 @@
 const request = require('postman-request');
 
-let place = 'Rome';
+let place = 'Seatides';
 const geoUrl = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + place +'.json?access_token=pk.eyJ1IjoiYXNodG9ubjc3IiwiYSI6ImNrOWVoNGJpZTAxdGozbnJ6YmZ4YTU3YmsifQ.URWLnzB_CFi-ArH9iLlShg&limit=1';
 
 request({url:geoUrl, json:true}, (error, response) => {
-    const data = response.body;
-    const latitude = data.features[0].center[1];
-    const longitude = data.features[0].center[0]
-    console.log(`Latitude = ${latitude}`);
-    console.log(`Longitude = ${longitude}`);
+    if(error){
+        console.log('Unable to connect to the location service');
+    }
+    else if(response.body.features.length < 1){
+        console.log('Unable to find location...check input and try again');
+    }
+    else{
+        const data = response.body;
+        const latitude = data.features[0].center[1];
+        const longitude = data.features[0].center[0]
+        console.log(`Latitude = ${latitude}`);
+        console.log(`Longitude = ${longitude}`);
+    }
 
 })
-
-
 
 // const url = 'http://api.weatherstack.com/current?access_key=1faba77a2a4bf8abdfeca8e98c4e2483&query=37.8267,-122.4233&units=m';
 
 // request({ url: url, json: true }, (error, response) => {
-//     const data = response.body.current;
-//     console.log(`${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. But it feels like ${data.feelslike} degrees out`);
-   
+//     if(error){
+//         console.log('Unable to connect to the weather service :(');
+//     }
+//     else if(response.body.error){
+//         console.log('Unable to find location :(');
+//     }
+//     else{
+//         const data = response.body.current;
+//         console.log(`${data.weather_descriptions[0]}. It is currently ${data.temperature} degrees out. But it feels like ${data.feelslike} degrees out`);
+//     }       
 // })
