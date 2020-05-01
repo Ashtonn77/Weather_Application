cd3f18c73c297d69fe9c39eeb72a206ebb993ef6
Ashtonn77 <ashtonrbay@gmail.com>
2020-05-01 19:21:17 +0200

    added public folder with assets

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,25 +1,10 @@
+const path = require('path');
 const express = require('express');
 
 const app = express();
+const publicDirectoryPath = path.join(__dirname, '../public');
 
-
-app.get('', (req, res) => {
-    res.send('<h1>My name is Ashton. I work at Google</h1>');
-})
-
-//help route
-app.get('/help', ( req, res ) => {
-    res.send({
-        name: 'Ashton',
-        company: 'Google',
-        age: '32'
-    });
-})
-
-//about
-app.get('/about', ( req, res ) => {
-    res.send('<h1 style="color:red;">So you want to know more about us</h1>');
-})
+app.use(express.static(publicDirectoryPath));
 
 //weather
 app.get('/weather', ( req, res ) => {
