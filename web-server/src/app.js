bf27ccbbc5f47733b4fc15ba9762351690d2cfe0
Ashtonn77 <ashtonrbay@gmail.com>
2020-05-02 18:30:17 +0200

    added templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,9 +2,39 @@ const path = require('path');
 const express = require('express');
 
 const app = express();
-const publicDirectoryPath = path.join(__dirname, '../public');
 
+//define paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public');
+const viewsPath = path.join(__dirname, '../templates');
+
+//setup handlebars engine and views location
+app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+
+
+//setup static directory to serve
 app.use(express.static(publicDirectoryPath));
+
+app.get('', ( req, res ) => {
+    res.render('index', {
+        title: 'Weather',
+        name: 'Ashton'
+    });
+})
+
+app.get('/about', ( req, res ) => {
+    res.render('about', {
+        title: 'About Me',
+        name: 'Ashton'
+    })
+})
+
+app.get('/help', ( req, res ) => {
+    res.render('help', {
+        message: 'How can we help you?',
+        name: 'Ashton'
+    })
+})
 
 //weather
 app.get('/weather', ( req, res ) => {
