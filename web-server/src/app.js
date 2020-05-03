3b3022277041e41c97187d19db606f120f11f9c8
Ashtonn77 <ashtonrbay@gmail.com>
2020-05-03 13:09:43 +0200

    updated templates

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,15 +1,18 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('hbs');
 
 const app = express();
 
 //define paths for express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-const viewsPath = path.join(__dirname, '../templates');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
 
 //setup handlebars engine and views location
 app.set('view engine', 'hbs');
 app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
 
 
 //setup static directory to serve
@@ -18,21 +21,22 @@ app.use(express.static(publicDirectoryPath));
 app.get('', ( req, res ) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Ashton'
+        name: 'Ashton Naidoo'
     });
 })
 
 app.get('/about', ( req, res ) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Ashton'
+        name: 'Ashton Naidoo'
     })
 })
 
 app.get('/help', ( req, res ) => {
     res.render('help', {
         message: 'How can we help you?',
-        name: 'Ashton'
+        title: 'Help',
+        name: 'Ashton Naidoo'
     })
 })
 
