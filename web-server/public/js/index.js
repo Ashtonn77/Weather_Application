055e09e00e61a562f1dee0645901e55ccbf9fd14
Ashtonn77 <ashtonrbay@gmail.com>
2020-05-07 20:06:33 +0200

    added js functionality

--- web-server/public/js/index.js
+++ web-server/public/js/index.js
@@ -1,1 +1,19 @@
 console.log('JS file has been loaded')
+
+fetch('/weather?address=Durban').then((response) => {
+    response.json().then((data) => {
+        if(data.error){
+            console.log(data.error)
+        }else{
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+        
+    })
+})
+
+const weatherForm = document.querySelector('form');
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    console.log('take one')
+})
